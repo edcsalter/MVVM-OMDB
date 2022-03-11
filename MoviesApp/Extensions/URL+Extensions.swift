cc9894ab5cad7701d0ee9d0205b3cf227995ef19
Ed Salter <edcsalter@gmail.com>
2022-03-11 12:47:41 -0500

    App completed

--- MoviesApp/Extensions/URL+Extensions.swift
+++ MoviesApp/Extensions/URL+Extensions.swift
@@ -9,6 +9,10 @@
 import Foundation
 
 extension URL {
+    static func forMoviesByImdbId(_ imdbId: String) -> URL? {
+        return URL(string: "https://www.omdbapi.com/?i=\(imdbId)&apikey=\(Constants.API_KEY)")
+    }
+    
     static func forMoviesByName(_ name: String) -> URL? {
         return URL(string: "https://omdbapi.com/?s=\(name)&apikey=\(Constants.API_KEY)")
     }
